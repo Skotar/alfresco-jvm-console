05b1882f522f1bcd8c6060b26f12b4181ab4ac82
Skotar <bzskotarek@gmail.com>
2019-12-12 10:45:35 +0100

    [added][alfresco] Test with useMainClassLoader [added][alfresco] Share to docker-compose [fixed][alfresco] Tests execution

--- alfresco/src/test/kotlin/pl/skotar/alfresco/module/jvmconsole/internal/ExecutorKotlinTest.kt
+++ alfresco/src/test/kotlin/pl/skotar/alfresco/module/jvmconsole/internal/ExecutorKotlinTest.kt
@@ -2,7 +2,7 @@ package pl.skotar.alfresco.module.jvmconsole.internal
 
 import io.kotlintest.matchers.instanceOf
 import io.kotlintest.shouldBe
-import io.kotlintest.shouldThrow
+import io.kotlintest.shouldThrowExactly
 import io.mockk.mockk
 import org.junit.Test
 import pl.skotar.alfresco.module.jvmconsole.internal.Executor.ClassByteCode
@@ -34,52 +34,63 @@ class ExecutorKotlinTest {
     @Test
     fun execute() {
         Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "execute", false)
+    }
+
+    @Test
+    fun `execute _ return string list`() {
+        Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "executeAndReturnStringList", false) shouldBe listOf("test")
+    }
+
+    @Test
+    fun `execute _ return integer list`() {
+        Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "executeAndReturnIntegerList", false) shouldBe listOf("-1")
+    }
+
+    @Test
+    fun `execute _ return string`() {
+        Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "executeAndReturnString", false) shouldBe listOf("test")
+    }
+
+    @Test
+    fun `executeNested _ return string`() {
+        Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "executeNestedAndReturnString", false) shouldBe listOf("test")
+    }
+
+    @Test
+    fun `executeLambda _ return string`() {
+        Executor(mockk(relaxed = true))
+            .execute(testClassByteCodes, className, "executeLambdaAndReturnString", false) shouldBe listOf("test")
+    }
+
+    @Test
+    fun `execute _ useMainClassLoader`() {
+        Executor(mockk(relaxed = true))
             .execute(testClassByteCodes, className, "execute", true)
-    }
 
-    @Test
-    fun executeAndReturnStringList() {
-        Executor(mockk(relaxed = true))
-            .execute(testClassByteCodes, className, "executeAndReturnStringList", true) shouldBe listOf("test")
-    }
-
-    @Test
-    fun executeAndReturnIntegerList() {
-        Executor(mockk(relaxed = true))
-            .execute(testClassByteCodes, className, "executeAndReturnIntegerList", true) shouldBe listOf("-1")
-    }
-
-    @Test
-    fun executeAndReturnString() {
-        Executor(mockk(relaxed = true))
-            .execute(testClassByteCodes, className, "executeAndReturnString", true) shouldBe listOf("test")
-    }
-
-    @Test
-    fun executeNestedAndReturnString() {
-        Executor(mockk(relaxed = true))
-            .execute(testClassByteCodes, className, "executeNestedAndReturnString", true) shouldBe listOf("test")
-    }
-
-    @Test
-    fun executeLambdaAndReturnString() {
-        Executor(mockk(relaxed = true))
-            .execute(testClassByteCodes, className, "executeLambdaAndReturnString", true) shouldBe listOf("test")
-    }
-
-    @Test
-    fun `executeWithParameter _ should throw IllegalStateException because function contains parameters`() {
-        shouldThrow<IllegalStateException> {
+        shouldThrowExactly<IllegalArgumentException> {
             Executor(mockk(relaxed = true))
-                .execute(testClassByteCodes, className, "executeWithParameter", true)
+                .execute(testClassByteCodes, className, "execute", true)
+        }.message shouldBe "Class is already loaded in class loader. Change name"
+    }
+
+    @Test
+    fun `executeWithParameter _ should throw IllegalArgumentException because function contains parameters`() {
+        shouldThrowExactly<IllegalArgumentException> {
+            Executor(mockk(relaxed = true))
+                .execute(testClassByteCodes, className, "executeWithParameter", false)
         }.message shouldBe "Class <${testClassByteCodes.first().canonicalClassName}> doesn't contain no-argument <executeWithParameter> function"
     }
 
     @Test
     fun `executeAndThrowException _ should throw InvocationTargetException that contains IllegalStateException`() {
-        shouldThrow<InvocationTargetException> {
+        shouldThrowExactly<InvocationTargetException> {
             Executor(mockk(relaxed = true))
-                .execute(testClassByteCodes, className, "executeAndThrowException", true)
+                .execute(testClassByteCodes, className, "executeAndThrowException", false)
         }.let {
             it.targetException shouldBe instanceOf(IllegalStateException::class)
             it.targetException.message shouldBe "I'm in incorrect state"
@@ -87,10 +98,10 @@ class ExecutorKotlinTest {
     }
 
     @Test
-    fun `noMatter _ should throw IllegalStateException because it contains constructor with parameters`() {
-        shouldThrow<IllegalStateException> {
+    fun `noMatter _ should throw IllegalArgumentException because it contains constructor with parameters`() {
+        shouldThrowExactly<IllegalArgumentException> {
             Executor(mockk(relaxed = true))
-                .execute(constructorWithParametersTestClassByteCodes, classWithParametersTestName, "noMatter", true)
+                .execute(constructorWithParametersTestClassByteCodes, classWithParametersTestName, "noMatter", false)
         }.message shouldBe "Class <${constructorWithParametersTestClassByteCodes.first().canonicalClassName}> doesn't contain no-argument constructor"
     }
 }
