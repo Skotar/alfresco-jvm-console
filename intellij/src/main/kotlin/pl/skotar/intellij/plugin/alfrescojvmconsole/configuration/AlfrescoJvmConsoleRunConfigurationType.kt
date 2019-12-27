dcc958d23bd56cf43f901b450075854fa777625f
Skotar <bzskotarek@gmail.com>
2019-12-27 21:22:09 +0100

    [added][intellij] Logo

--- intellij/src/main/kotlin/pl/skotar/intellij/plugin/alfrescojvmconsole/configuration/AlfrescoJvmConsoleRunConfigurationType.kt
+++ intellij/src/main/kotlin/pl/skotar/intellij/plugin/alfrescojvmconsole/configuration/AlfrescoJvmConsoleRunConfigurationType.kt
@@ -2,9 +2,12 @@ package pl.skotar.intellij.plugin.alfrescojvmconsole.configuration
 
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object AlfrescoJvmConsoleRunConfigurationType : ConfigurationType {
+
+    private val icon = IconLoader.getIcon("/icon/alfresco.svg", AlfrescoJvmConsoleRunConfigurationType::class.java)
 
     override fun getDisplayName(): String =
         "Alfresco JVM Console"
@@ -12,8 +15,8 @@ object AlfrescoJvmConsoleRunConfigurationType : ConfigurationType {
     override fun getConfigurationTypeDescription(): String? =
         null
 
-    override fun getIcon(): Icon? =
-        null
+    override fun getIcon(): Icon =
+        icon
 
     override fun getId(): String =
         "ALFRESCO_JVM_CONSOLE_RUN_CONFIGURATION"
